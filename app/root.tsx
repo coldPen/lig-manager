6a029e41cd26333f076627b33978903c15c6e991
Lucas <lucas.leroux@pm.me>
2024-12-23 17:17:52 +0100

    Schema + seed init

--- app/root.tsx
+++ app/root.tsx
@@ -5,10 +5,21 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "react-router";
+} from "react-router"
 
-import type { Route } from "./+types/root";
-import stylesheet from "./app.css?url";
+import type { Route } from "./+types/root"
+import stylesheet from "./app.css?url"
+
+// For reference, for later use in loaders and actions
+export const AttendanceType = {
+  REGULAR: "REGULAR",
+  VISITOR: "VISITOR",
+} as const
+export const AttendanceStatus = {
+  PLANNED: "PLANNED",
+  ABSENT: "ABSENT",
+  CANCELLED: "CANCELLED",
+} as const
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -22,11 +33,11 @@ export const links: Route.LinksFunction = () => [
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
   { rel: "stylesheet", href: stylesheet },
-];
+]
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="fr">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -39,38 +50,38 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Scripts />
       </body>
     </html>
-  );
+  )
 }
 
 export default function App() {
-  return <Outlet />;
+  return <Outlet />
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
-  let message = "Oops!";
-  let details = "An unexpected error occurred.";
-  let stack: string | undefined;
+  let message = "Oups!"
+  let details = "Une erreur inattendue s'est produite."
+  let stack: string | undefined
 
   if (isRouteErrorResponse(error)) {
-    message = error.status === 404 ? "404" : "Error";
+    message = error.status === 404 ? "404" : "Erreur"
     details =
       error.status === 404
-        ? "The requested page could not be found."
-        : error.statusText || details;
+        ? "La page demandée n'a pas été trouvée."
+        : error.statusText || details
   } else if (import.meta.env.DEV && error && error instanceof Error) {
-    details = error.message;
-    stack = error.stack;
+    details = error.message
+    stack = error.stack
   }
 
   return (
-    <main className="pt-16 p-4 container mx-auto">
+    <main>
       <h1>{message}</h1>
       <p>{details}</p>
       {stack && (
-        <pre className="w-full p-4 overflow-x-auto">
+        <pre>
           <code>{stack}</code>
         </pre>
       )}
     </main>
-  );
+  )
 }
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,8 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { prisma } from "~/utils/db.server"
+import type { Route } from "./+types/home"
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
+export async function loader() {}
 
 export default function Home() {
-  return <Welcome />;
+  return <></>
 }
